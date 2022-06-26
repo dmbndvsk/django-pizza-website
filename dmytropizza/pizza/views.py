0f9655cca9f1064b3749239cb6e7781f00de0451
Dmytro Bendovskyi <dmytrobendovskyi1990@gmail.com>
2022-06-26 17:08:58 +0300

    forms.py modified with class Meta

--- dmytropizza/pizza/views.py
+++ dmytropizza/pizza/views.py
@@ -12,9 +12,9 @@ def order(request):
     if request.method == 'POST':
         filled_form = PizzaForm(request.POST)
         if filled_form.is_valid():
-            note = 'Thanks for ordering! Your %s %s and %s Pizza is on its way!' %(filled_form.cleaned_data['size'],
-                                                                                  filled_form.cleaned_data['topping1'],
-                                                                                  filled_form.cleaned_data['topping2'])
+            note = 'Thanks for ordering! Your %s %s and %s Pizza is on its way!' % (filled_form.cleaned_data['size'],
+                                                                                    filled_form.cleaned_data['topping1'],
+                                                                                    filled_form.cleaned_data['topping2'])
             new_form = PizzaForm()
             return render(request, 'pizza/order.html', {'pizzaform': new_form, 'note': note})
     else:
